f12e117b534c597ad4ccfa5b606597e108aac1aa
Lavanya Sood <lavanyasood09@gmail.com>
2022-07-25 17:21:13 +1000

    Added base website code

--- lavanya-sood.github.io/src/Contact.js
+++ lavanya-sood.github.io/src/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import emailjs from "emailjs-com";
+import mailIcon from './images/mail-xxl.png';
+import githubIcon from './images/github.png';
+import linkedIcon from './images/linkedin.png';
 
 const Contact = (props) => {
 
@@ -7,7 +9,13 @@ const Contact = (props) => {
   return (
     <div className="page-body" id="aboutpage">
         <h1 className='page-headings'> Contact Me </h1>
-        <p>Please reach out to me using any of the following methods</p>
+        <p>Please reach out to me using any of the following methods:</p>
+        <div id="contacts">
+          <a href="mailto:lavanyasood09@gmail.com"><img src={mailIcon} className='contact-image'/></a>
+          <a href="http://github.com/lavanya-sood"><img src={githubIcon} className='contact-image'/></a>
+          <a href="http://www.linkedin.com/in/lavanya-sood/"><img src={linkedIcon} className='contact-image'/></a>
+        </div>
+
     </div>
     
   );
